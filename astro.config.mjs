29ad0437b749dabf6065afdd70738a0e635ea77e
Prabhav Dogra <prabhavdogra1@gmail.com>
2025-06-08 10:54:14 +0530

    added support for titles in code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import alpinejs from '@astrojs/alpinejs';
 import { remarkAlert } from "remark-github-blockquote-alert";
+import astroExpressiveCode from 'astro-expressive-code'
 
 export default defineConfig({
   site: 'https://github.com/prabhavdogra',
   base: '/',  
   integrations: [
     tailwind(),
-    alpinejs()
+    alpinejs(),
+    astroExpressiveCode({
+      shiki: { theme: 'one-dark-pro', wrap: true },
+    }),
   ],
   markdown: {
     remarkPlugins: [ 
